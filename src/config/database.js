5727a0ceec922299b3bdc63a652cc80ead342a6c
Henrique Augusto <hick_97@hotmail.com>
2019-09-02 14:22:55 -0300

    Updating .env.example

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
 
 module.exports = {
-  uri: `mongodb://${process.env.DB_MONGO_USER}:${process.env.DB_MONGO_PASS}@ds257848.mlab.com:57848/${process.env.DB_MONGO_NAME}`
+  uri: process.env.DB_MONGO_URI
 }
